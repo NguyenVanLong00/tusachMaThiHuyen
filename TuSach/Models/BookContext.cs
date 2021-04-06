d686455824642f606d3d65708585964f48a7450f
NguyenVanLong <nguyenvanlong0x0@gmail.com>
2021-04-06 19:56:52 +0700

    commit

--- TuSach/Models/BookContext.cs
+++ TuSach/Models/BookContext.cs
@@ -10,6 +10,7 @@ namespace TuSach.Models
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Book> Books { get; set; }
+
         public DbSet<User> Users { get; set; }
 
         
--- TuSach/Models/User.cs
+++ TuSach/Models/User.cs
@@ -12,8 +12,6 @@ namespace TuSach.Models
 
        [Required, StringLength(15)]
        public string Password { get; set; }
-
-       
        
 
     }
